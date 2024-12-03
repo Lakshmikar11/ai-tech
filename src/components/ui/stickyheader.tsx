dba0ab6e1cfe882dabc6c91ce7734a5d6f3cdd04
Lakshmikar Reddy <lakshmikarreddy30@gmail.com>
2024-12-03 13:09:53 +0530

    stickyheader modified

--- src/components/ui/stickyheader.tsx
+++ src/components/ui/stickyheader.tsx
@@ -1,18 +1,49 @@
 import { ModeToggle } from "./modetoggle"
+import { Button } from "./button"
+import {
+  Menubar,
+  MenubarContent,
+  MenubarItem,
+  MenubarMenu,
+  MenubarTrigger,
+} from "@/components/ui/menubar"
+
 export default function Stickyheader(){
     return(
 <>
     <header className="sticky top-0 z-50 bg-white bg-opacity-80 shadow-md backdrop-blur-md">
       <div className="max-w-7xl mx-auto p-4 flex items-center justify-between">
         <h1 className="text-lg font-bold">Halix Tech Solutions</h1>
-        <nav className="flex space-x-12 mx-auto">
-          <a href="#home" className="text-gray-600 hover:text-gray-900">Home</a>
-          <a href="#services" className="text-gray-600 hover:text-gray-900">Services</a>
-          <a href="#about" className="text-gray-600 hover:text-gray-900">About Us</a>
-          <a href="#contact" className="text-gray-600 hover:text-gray-900">Contact</a>
-          <a href="#contact" className="text-gray-600 hover:text-gray-900">Portfolio</a>
+        <nav className="hidden md:flex space-x-6">
+        <a href="/" className="text-muted-foreground hover:text-primary">Home</a>
+          <a href="/about" className="text-muted-foreground hover:text-primary">About</a>
+          <a href="/services" className="text-muted-foreground hover:text-primary">Services</a>
+          <a href="/contact" className="text-muted-foreground hover:text-primary">Contact</a>
         </nav>
         <ModeToggle />
+      <Menubar>
+        <MenubarMenu>
+            <MenubarTrigger asChild className="md:hidden">
+              <Button variant="ghost">
+                <span>☰</span>
+              </Button>
+            </MenubarTrigger>
+          <MenubarContent>
+            <MenubarItem>
+              <a href="/">Home</a>
+            </MenubarItem>
+            <MenubarItem>
+              <a href="/about">About</a>
+            </MenubarItem>
+            <MenubarItem>
+              <a href="/services">Services</a>
+            </MenubarItem>
+            <MenubarItem>
+              <a href="/contact">Contact</a>
+            </MenubarItem>
+          </MenubarContent>
+        </MenubarMenu>
+      </Menubar>
       </div>
     </header>
 
